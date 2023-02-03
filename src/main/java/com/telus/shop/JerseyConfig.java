f71a9e081e3eb6e0b60c90ceab8af02fb5513f8f
Wilfredo Alfaro <wilfredo.alfaro@telusinternational.com>
2023-02-03 12:07:55 -0600

    Set Jersey config

---
+++ src/main/java/com/telus/shop/JerseyConfig.java
@@ -0,0 +1,12 @@
+package com.telus.shop;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        register(ItemService.class);
+    }
+}
